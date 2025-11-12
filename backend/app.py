0c80fefa3493a2681cfb033b1a6f64d97ae77da3
Om Amar <emailomamar@gmail.com>
2025-11-12 21:22:47 +0530

    updated

--- backend/app.py
+++ backend/app.py
@@ -232,4 +232,5 @@ if __name__ == "__main__":
             db.session.add(new_admin)
             db.session.commit()
 
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
